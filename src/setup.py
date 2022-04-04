372f05a823a91df522e6946b0fefd08108be3947
unknown <shechteridan790@gmail.com>
2022-04-04 20:25:02 +0300

    Added setup.py, responsible for creating a UUID and stuff

---
+++ src/setup.py
@@ -0,0 +1,24 @@
+import json
+import os.path
+import uuid
+
+
+def generate_uuid():
+    return str(uuid.uuid1())
+
+
+def sanity():
+
+    if os.path.exists("info.json"):
+        with open("info.json", "r") as setup:
+            data = json.load(setup)
+            unique = data.get("uuid")
+    else:
+        unique = generate_uuid()
+        data = {
+            "uuid": f"{unique}"
+        }
+        with open("info.json", "w+") as setup:
+            json.dump(data, setup)
+
+    return unique
