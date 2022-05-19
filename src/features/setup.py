e05b7dc5150316ccb0faa4ada4c6e3b51655ab93
Zethus <ravet.zvika93@gmail.com>
2022-05-19 19:52:25 +0300

    redundent file name

--- src/features/setup.py
+++ src/features/setup.py
@@ -6,10 +6,6 @@ from dotenv import load_dotenv
 import os as osVars
 from discord.ext import commands
 
-
-__DISCORD_CHANNELS_UUID_FILE = "info.json"
-
-
 # generate uuid using the target's mac address
 def generate_uuid():
     return str(uuid.getnode())
