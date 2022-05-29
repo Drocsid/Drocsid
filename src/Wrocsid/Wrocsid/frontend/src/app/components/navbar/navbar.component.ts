2b2eb4b57afdafeb01d7c774bb5719fdc65e5e7f
Zethus <ravet.zvika93@gmail.com>
2022-05-29 12:46:42 +0300

    website logo that sends you to the discord c2 guild

--- src/Wrocsid/Wrocsid/frontend/src/app/components/navbar/navbar.component.ts
+++ src/Wrocsid/Wrocsid/frontend/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment  } from '../../../environments/environment'
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  invite_link: string = ""
+
   constructor() { }
 
   ngOnInit(): void {
+    this.invite_link = 'https://discord.gg/'.concat(environment.INVITE_ID)
   }
-
 }
